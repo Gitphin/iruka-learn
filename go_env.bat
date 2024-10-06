3b4f6ade463b7dc1a01d74b0b7cfb0c6c3ca841d
Gitphin <alek.holiman@gmail.com>
2024-10-06 15:10:47 -0500

    Started my project

--- go_env.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-if exist "myenv\Scripts\activate.bat" (
-    call myenv\Scripts\activate.bat
-    echo Virtual environment activated.
-) else (
-    echo Virtual environment not found.
-)
-
